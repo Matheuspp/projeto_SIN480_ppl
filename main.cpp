a7b0a7daf6c53529910e04c630f4a307ab3aa4ad
Matheus Silva <matheus.silva@acertpix.com.br>
2022-03-27 04:01:32 -0300

    update data entry / add to main

--- main.cpp
+++ main.cpp
@@ -15,18 +15,45 @@ int main(int argc, char *argv[])
 {
 
     // Declarando conjuntos de entrada
-    int F; // conjunto de fazendas
-    int C; // conjunto de culturas
-    
-    // Dados de entrada dependentes dos conjuntos
-    char** NomeFazenda; // por fazenda
-    float* Area; // por fazenda
-    float* Agua; // por fazenda
+    int C; // conjunto de componentes
+    int E; // conjunto de especialistas
 
-    char** NomeCultura; // por Cultura
-    float* AMax; // por cultura
-    float* ConsAgua; // por cultura
-    float* Lucro; // por cultura    
+    // Dados de entrada dependentes dos conjuntos
+    char** NomeComponente; // por componente
+    char** NomeCelula; // por componente
+    char** NomeServico; // por compenente
+    float* Desmontagem; // por componente
+    float* Limpeza; // por componente
+    float* Vdi; // por componente
+    float* End; // por componente
+    float* Protecao; // por componente
+    float* Pintura; // por componente
+    float* Conf_pecas; // por componente
+    float* Lib_montagem; // por componente
+    float* Montagem; // por componente
+    float* InspecaoFI; // por componente
+    float* Ensaio; // por componente
+    float* Montagem_final; // por componente
+    float* Pintura_final; // por componente
+    float* Finalizacao; // por componente
+    float* Cf; // por componente
+    float* HH_total; // por componente
+    float* Demanda; // por componente
+
+    char** NomeEspec; // por especialista
+    char** Mec_leve; // por especialista
+    char** Mec_medio; // por especialista
+    char** Mec_pesado; // por especialista
+    char** Insp_leve; // por especialista
+    char** Insp_medio; // por especialista
+    char** Insp_pesado; // por especialista
+    char** End_esp; // por especialista
+    char** Limpeza_esp; // por especialista
+    char** Protecao_esp; // por especialista
+    char** Pintura_esp; // por especialista
+
+
+    float* TotalHorasServico; // especialidade x componentes
 
     // Leitura do Arquivo
     FILE* fp;
@@ -35,25 +62,52 @@ int main(int argc, char *argv[])
     if( fp == NULL)
     {
         printf("Erro ao abrir o arquivo!\n");
-        exit(1);    
+        exit(1);
     }
-    
+
     // Leitura dos conjuntos
-    fscanf(fp, "%d", &F);
     fscanf(fp, "%d", &C);
+    fscanf(fp, "%d", &E);
 
     // Definindo os dados de entrada de acordo com o tamanho dos conjuntos lidos
-    // Por fazenda
-    NomeFazenda = new char*[F];
-    for(int f; f < F; f++)
+    // Por Componente
+    NomeComponente = new char*[C];
+    for(int c; c < C; c++)
     {
-      NomeFazenda[f] = new char[51];
+      NomeComponente[c] = new char[51];
     }
 
-    Area = new float[F];
-    Agua = new float[F];
+    NomeServico = new char*[C];
+    for(int c; c < C; c++)
+    {
+      NomeServico[c] = new char[51];
+    }
 
-    // Por cultura
+    NomeCelula = new char*[C];
+    for(int c; c < C; c++)
+    {
+      NomeCelula[c] = new char[51];
+    }
+
+    Desmontagem = new float[C];
+    Limpeza = new float[C];
+    Vdi = new float[C];
+    End = new float[C];
+    Protecao = new float[C];
+    Pintura = new float[C];
+    Conf_pecas = new float[C];
+    Lib_montagem = new float[C];
+    Montagem = new float[C];
+    InspecaoFI = new float[C];
+    Ensaio = new float[C];
+    Montagem_final = new float[C];
+    Pintura_final = new float[C];
+    Finalizacao= new float[C];
+    Cf = new float[C];
+    HH_total = new float[C];
+    Demanda = new float[C]
+
+    // Por especialidade continue....
     NomeCultura = new char*[C];
     for(int c = 0 ; c < C; c++)
     {
@@ -71,8 +125,8 @@ int main(int argc, char *argv[])
     {
         fscanf(fp, "%s", NomeFazenda[f]);
         fscanf(fp, "%f", &Area[f]);
-        fscanf(fp, "%f", &Agua[f]);   
-    }    
+        fscanf(fp, "%f", &Agua[f]);
+    }
 
     // por cultura
     for(int c = 0; c < C; c++)
@@ -84,7 +138,7 @@ int main(int argc, char *argv[])
     }
 
     // Impressão para Verificação dos dados
-    
+
 
     printf("Numero de faz: %d\n", F);
     printf("Numero de cult: %d\n\n", C);
@@ -110,11 +164,11 @@ int main(int argc, char *argv[])
 	  IloModel modelo;
     // iniciando o modelo
     modelo = IloModel(env);
-    
 
-    
+
+
     /*
-    // ---------------  DECLARAÇÃO DE VARIÁVEIS DE UMA DIMENSÃO    
+    // ---------------  DECLARAÇÃO DE VARIÁVEIS DE UMA DIMENSÃO
     // parametros para declaração de variáveis: ambiente, tamanho, valor min, valor max, tipo (ILOINT, ILOFLOAT, ILOBOOL)
     IloNumVarArray y(env,TAM, 0, IloInfinity, ILOFLOAT); //  y >= 0
 
@@ -164,7 +218,7 @@ int main(int argc, char *argv[])
     }
 
 
-    
+
 
     // DECLARAÇÃO DA FUNÇÃO OBJETIVO
 
@@ -179,16 +233,16 @@ int main(int argc, char *argv[])
       }
     }
 
-      
+
     //IloMinimize e IloMaximize
     modelo.add(IloMaximize(env, fo));
 
-    
+
     // DECLARAÇÃO DAS RESTRIÇÕES DO PROBLEMA
 
     // declarando a restrição
     // IloRange parametros: ambiente, valor min, expressão, valor maximo
-    
+
     // Restrição associada a area da fazenda
     for(int f = 0; f < F; f++) //para todo
     {
@@ -201,15 +255,15 @@ int main(int argc, char *argv[])
       IloRange areaFaz(env, -IloInfinity, soma, Area[f]);
       // dando um nome para a restrição
       stringstream rest;
-      rest << " AreaFaz[" << NomeFazenda[f] << "]: "; 
+      rest << " AreaFaz[" << NomeFazenda[f] << "]: ";
       areaFaz.setName(rest.str().c_str());
       //adicionar ao modelo
-      modelo.add(areaFaz);       
+      modelo.add(areaFaz);
 
     }
 
     // associada a água disponivel nas Fazendas
-    
+
     for(int f = 0; f < F; f++)
     {
        IloExpr soma(env);
@@ -225,7 +279,7 @@ int main(int argc, char *argv[])
        rest_consAgua.setName(rest.str().c_str());
        // adiciono ao modelo
       modelo.add(rest_consAgua);
-      
+
     }
 
 
@@ -245,7 +299,7 @@ int main(int argc, char *argv[])
         rest_areaCultura.setName(rest.str().c_str());
         //adicionar ao modelo
         modelo.add(rest_areaCultura);
-        
+
     }
 
     // restrição associada a proporção de area plantada
@@ -260,7 +314,7 @@ int main(int argc, char *argv[])
 
               for(int c = 0 ; c < C; c++)
               {
-                  soma1+=x[f][c];            
+                  soma1+=x[f][c];
                   soma2+=x[g][c];
               }
               //declarar minha restrição
@@ -283,9 +337,9 @@ int main(int argc, char *argv[])
     cplex.exportModel("fazenda.lp");
     // Executando o modelo
     cplex.solve();
-    
-    // PEGAR OS VALORES DAS VARIÁVEIS 
-    //cplex.getValue(NOME_VAR)    
+
+    // PEGAR OS VALORES DAS VARIÁVEIS
+    //cplex.getValue(NOME_VAR)
     printf("\n ---------- Valor das variaveis -------------\n");
     for(int f = 0; f < F; f++)
     {
